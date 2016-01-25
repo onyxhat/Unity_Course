b8f276cb633cdbd9bae7e92299de635d1011fed5
Isaac Springer <idspringer@onyxhat.com>
2016-01-25 08:56:04 +0530

    Added Awake method in Musicplayer

--- Brick Breaker/Assets/Scripts/MusicPlayer.cs
+++ Brick Breaker/Assets/Scripts/MusicPlayer.cs
@@ -5,17 +5,20 @@ public class MusicPlayer : MonoBehaviour
 {
 	static MusicPlayer instance = null;
 
-	// Use this for initialization
-	void Start ()
+	// Awake() is called before Start()
+	void Awake ()
 	{
+		Debug.Log ("MusicPlayer Awake: " + GetInstanceID ());
+
 		if (instance != null) {
+			Debug.Log ("Destroying Duplicate MusicPlayer Instance: " + GetInstanceID ());
 			Destroy (gameObject);
 		} else {
 			instance = this;
 			GameObject.DontDestroyOnLoad (gameObject);
 		}
 	}
-	
+
 	// Update is called once per frame
 	void Update ()
 	{
