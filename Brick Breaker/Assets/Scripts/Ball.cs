89dd686ae2d4eea9cd0fecaeea197dad03a70b19
Isaac Springer <idspringer@onyxhat.com>
2016-01-26 12:34:41 +0530

    add ball and paddle physics

---
+++ Brick Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour
+{
+	public Paddle paddle;
+	private Vector3 paddleToBallVector;
+	private bool hasStarted = false;
+
+	// Use this for initialization
+	void Start ()
+	{
+		paddleToBallVector = this.transform.position - paddle.transform.position;
+		Debug.Log ("P2B Vector: " + paddleToBallVector);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (!hasStarted) {
+			//Lock ball relative to paddle
+			this.transform.position = paddle.transform.position + paddleToBallVector;
+
+			//Wait for click to launch ball
+			if (Input.GetMouseButtonDown (0)) {
+				Debug.Log ("Ball Release");
+				hasStarted = true;
+				this.rigidbody2D.velocity = new Vector2 (0f, 10f);
+			}
+		}
+	}
+}
---
+++ Brick Breaker/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour
+{
+	public LevelManager levelManager;
+
+	void OnTriggerEnter2D (Collider2D trigger)
+	{
+		Debug.Log ("Trigger: " + GetInstanceID ());
+		levelManager.LoadLevel ("Lose");
+	}
+
+	void OnCollisionEnter2D (Collision2D collision)
+	{
+		Debug.Log ("Collision: " + GetInstanceID ());
+	}
+}
